517e5bd204d03c49321371698878d7063a800e07
Wendel Luan <wendelluan@alu.ufc.br>
2024-02-24 10:55:50 -0300

    api working(organization code update)

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -15,25 +15,27 @@ export async function fetchChatGPTResponse(
     typeFormatted
   );
 
+  console.log(`instrução: ${instrucao}`);
+
   try {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: `${question}` }],
+        messages: [{ role: "user", content: `${instrucao} ${question}` }],
         temperature: 0.7,
       },
       {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${OPENAI_API_KEY}`,
+          "OpenAI-Organization": 'org-5wwmS0Gdvnr4XIohz3YQS8Ko',
         },
       }
     );
-    console.log("Entrou no chat controler")
+    
     return response.data.choices[0].message.content;
   } catch (error) {
-    console.log(error)
     console.error("Erro ao buscar resposta do ChatGPT:", error);
     throw error;
   }
